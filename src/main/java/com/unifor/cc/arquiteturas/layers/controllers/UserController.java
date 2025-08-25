895e8a230e7128acc0a38a173a7d4bce5e04aa34
Cassio Castro <ccastro@Cassios-MacBook-Air.local>
2025-08-25 20:38:46 -0300

    Refactor: improve UserController and UserService, update build.gradle - Enhanced UserController with constructor injection, logging, error handling, and documentation. - Improved UserService with Lombok @RequiredArgsConstructor, logging, null checks, and JavaDoc. - Updated build.gradle (see diff for details).

--- src/main/java/com/unifor/cc/arquiteturas/layers/controllers/UserController.java
+++ src/main/java/com/unifor/cc/arquiteturas/layers/controllers/UserController.java
@@ -1,11 +1,39 @@
 package com.unifor.cc.arquiteturas.layers.controllers;
 
+import com.unifor.cc.arquiteturas.layers.models.User;
 import com.unifor.cc.arquiteturas.layers.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
-    private UserService userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody User user) {
+        logger.info("Received request to create user: {}", user);
+        Optional<User> newUser = userService.save(user);
+        if (newUser.isPresent()) {
+            return ResponseEntity.ok(newUser.get());
+        } else {
+            logger.warn("User could not be created: {}", user);
+            return ResponseEntity.badRequest().body("User could not be created");
+        }
+    }
 
 }
--- src/main/java/com/unifor/cc/arquiteturas/layers/services/UserService.java
+++ src/main/java/com/unifor/cc/arquiteturas/layers/services/UserService.java
@@ -1,13 +1,35 @@
 package com.unifor.cc.arquiteturas.layers.services;
 
+import com.unifor.cc.arquiteturas.layers.models.User;
 import com.unifor.cc.arquiteturas.layers.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    private UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+    private final UserRepository userRepository;
 
+    public Optional<User> findById(Long id) {
+        if (id == null) {
+            logger.warn("Attempted to find user with null id");
+            return Optional.empty();
+        }
+        return userRepository.findById(id);
+    }
 
+    public Optional<User> save(User user) {
+        if (user == null) {
+            logger.warn("Attempted to save null user");
+            return Optional.empty();
+        }
+        return Optional.of(userRepository.save(user));
+    }
 
 }
